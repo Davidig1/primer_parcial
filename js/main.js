19e0e5df6bd9f46c6c0b845dbe4a112c02de1ecd
Davidig1 <dvisglap@gmail.com>
2020-04-16 23:28:39 -0400

    primer commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(init);
+
+function init(){
+    $("#mi_libro").booklet(
+        {
+            width:900,
+            height:800,
+
+            closed: true,
+            covers: true,
+            autoCenter: true,
+
+            pagePaddin: 0,
+            hoverWidth: 100,
+        }
+    );
+    $("#bt_go").click(function(){
+        $("#mi_libro").booklet("gotopage",
+        $("#in_go").val());
+    });
+}
+
